4829f7dd8f91e0233ceef3b69092faf12a2a0e11
lingling <1554631500@qq.com>
2021-03-31 18:20:25 +0800

    3

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,11 +2,11 @@ const mysql=require('mysql');
 module.exports={
     DB(sql,arr,fn){
         let db=mysql.createConnection({
-            // host:"localhost",
-            // port:3306,
-            // user:'root',
-            // password:'1234',
-            // database:'dadasql'
+            host:"172.16.10.60",
+            port:3306,
+            user:'root',
+            password:'1234',
+            database:'pro3data'
         });
         db.connect();
         db.query(sql,arr,fn);
