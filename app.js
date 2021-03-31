f986fb771163b59fbb61554dd0d2abf1ca7b97ed
lingling <1554631500@qq.com>
2021-03-31 19:33:20 +0800

    666

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const express=require('express');
-const logger=require('morgan');
-const favicon=require('serve-favicon');
-const LoginRouter=require('./router/LoginRouter');
-const cartRouter=require('./router/cartRouter');
-const manRouter=require('./router/manRouter');
-const orderRouter=require('./router/orderRouter');
-const womanRouter=require('./router/womanRouter');
-const yyRouter=require('./router/yyRouter');
+const express = require('express');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
+const session = require('express-session');
+const LoginRouter = require('./router/LoginRouter');
+const cartRouter = require('./router/cartRouter');
+const manRouter = require('./router/manRouter');
+const orderRouter = require('./router/orderRouter');
+const womanRouter = require('./router/womanRouter');
+const yyRouter = require('./router/yyRouter');
 
 
 
-const app=express();
+const app = express();
 
 app.use(logger('dev'));
 
+app.use(session({
+    name: 'pro3data',
+    secret: '1234',
+    cookie: {
+        maxAge: 6000
+    },
+    resave: true,//每次请求都重置session cookie
+    saveUninitialized: true,//无论有没有session都会设置一个默认空对象
+}))
+
 app.use(express.json());
 app.use(express.urlencoded({
-    extended:true
+    extended: true
 }));
 // ruter引用处
 app.use(LoginRouter);
@@ -26,9 +37,9 @@ app.use(orderRouter);
 app.use(womanRouter);
 app.use(yyRouter);
 
-app.use(express.static(__dirname+'/src'));
-app.use(favicon(__dirname+'/src/images/view_3.png'));
+app.use(express.static(__dirname + '/src'));
+app.use(favicon(__dirname + '/src/images/view_3.png'));
 
-app.listen(8888,()=>{
+app.listen(8888, () => {
     console.log('第三组项目服务器开启~');
 })
