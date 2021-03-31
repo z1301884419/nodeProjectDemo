34d7ea6c4aa1e5e6395a334b92dd565488441333
qianwang <727767815@qq.com>
2021-03-31 19:34:10 +0800

    7

--- router/cartRouter.js
+++ router/cartRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const router = require('../../../第三阶段/LOGIN/router/router');
 const cartRouter = express.Router();
 const cartcontrol = require('../controller/cartCtrl');
 
+router.get('/cartInfo', cartcontrol.cartInfo);
 
-module.exports = cartRouter
+
+module.exports = cartRouter;
