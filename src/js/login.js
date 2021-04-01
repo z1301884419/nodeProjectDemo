b6b9c912d874e0edc821e46196ec258dc6254c7a
lingling <1554631500@qq.com>
2021-04-01 15:48:33 +0800

    444

--- src/js/login.js
+++ src/js/login.js
@@ -12,10 +12,23 @@ $('.loginTrue').on('click',function(){
         dataType:'JSON',
         success:function(data){
             if(data.code==200){
-                console.log(data.data.u_name);
-                $('.loginBtn').css('display','none');
+                $('.loginBox2').css('display','none');
                 $('.userNameInfo').css('display','block');
                 $('.userNameInfo').text(data.data.u_name);
+                $('.userNode').html(`<div class="userCenter">
+                <div class="userHeader">
+                    <img src="./upload/6.jpg" alt="">
+                </div>
+                <div class="userNameBox">
+                    <input type="text" value="${data.data.u_name}" disabled='disabled'><span class="iconfont icon-bi shou modifyName"></span>
+                </div>
+                <div class="caozuo">
+                    <div><span class="iconfont icon-shoucang"></span>我的收藏</div>
+                    <div><span class="iconfont icon-dingdan1"></span>我的订单</div>
+                    <div><span class="iconfont icon-gouwuche"></span>我的购物车</div>
+                    <div><span class="iconfont icon-zuji"></span>我浏览的</div>
+                </div>
+            </div>`)
             }
         },
         err:function(err){
@@ -24,5 +37,8 @@ $('.loginTrue').on('click',function(){
     });
 });
 $('.userNameInfo').on('mouseover',function(){
-    
+    $('.userCenter').css('opacity','1');
+});
+$('.info').on('mouseleave',function(){
+    $('.userCenter').css('opacity','0');
 })
