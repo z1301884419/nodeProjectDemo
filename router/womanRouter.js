908d77dab4a1b0fd3135df993ac0ea841c1974be
zbw <247155703@qq.com>
2021-04-02 17:04:05 +0800

    郑博文

--- router/womanRouter.js
+++ router/womanRouter.js
@@ -1,4 +1,8 @@
-const express =require('express');
-const womanRouter=express.Router();
-
-module.exports=womanRouter
+const express = require('express');
+const womanCtrl = require('../controller/womanCtrl');
+const womanRouter = express.Router();
+womanRouter.get('/selectWoman', womanCtrl.selectWoman)
+womanRouter.get('/selectWomanImg', womanCtrl.selectWomanImg)
+womanRouter.get('/selectClass', womanCtrl.selectClass)
+womanRouter.get('/selectPage', womanCtrl.selectPage)
+module.exports = womanRouter
